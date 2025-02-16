ef7ac01bda812f54b9ae02989faadbf7157f97d5
Matija Osrečki <github@matijao.com>
2025-02-16 16:15:59 +0100

    refac: downgrade to tailwind 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,12 @@
-import tailwindcss from '@tailwindcss/vite'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@vueuse/nuxt'],
+  modules: ['@nuxt/eslint', '@vueuse/nuxt', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-11-01',
   future: {
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [tailwindcss()],
-  },
   vueuse: {
     autoImports: true,
   },
